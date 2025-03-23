b1025ab93c0379f10648b78c66c57ebd907c3a97
inv3r53 <inv3r53@users.noreply.github.com>
2025-03-23 11:46:46 +1100

    Add LÖVE configuration file

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.title = "Pong"
+    t.version = "11.4"
+    t.window.width = 800
+    t.window.height = 600
+    t.window.resizable = false
+    t.window.vsync = true
+    
+    -- For debugging
+    t.console = true
+end
